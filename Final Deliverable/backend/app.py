915c40cf561e74a964447c58e85627744f000c47
SWETHA-7751 <108478895+SWETHA-7751@users.noreply.github.com>
2022-11-19 16:46:46 +0530

    Create app.py

---
+++ Final Deliverable/backend/app.py
@@ -0,0 +1,197 @@
+import news
+from flask import Flask, render_template, request, jsonify, session
+import datetime
+import re
+
+
+
+import ibm_db
+import pandas
+import ibm_db_dbi
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite://',
+                       echo = False)
+
+dsn_hostname = "19af6446-6171-4641-8aba-9dcff8e1b6ff.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud"
+dsn_uid = "wdn20062"
+dsn_pwd = "2eFT80IyOuJQE3zV"
+
+dsn_driver = "{IBM DB2 ODBC DRIVER}"
+dsn_database = "bludb"
+dsn_port = "30699"
+dsn_protocol = "TCPIP"
+dsn_security = "SSL"
+
+dsn = (
+    "DRIVER={0};"
+    "DATABASE={1};"
+    "HOSTNAME={2};"
+    "PORT={3};"
+    "PROTOCOL={4};"
+    "UID={5};"
+    "PWD={6};"
+    "SECURITY={7};").format(dsn_driver, dsn_database, dsn_hostname, dsn_port, dsn_protocol, dsn_uid, dsn_pwd,dsn_security)
+
+
+
+try:
+    conn = ibm_db.connect(dsn, "", "")
+    print ("Connected to database: ", dsn_database, "as user: ", dsn_uid, "on host: ", dsn_hostname)
+
+except:
+    print ("Unable to connect: ", ibm_db.conn_errormsg() )
+
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+
+
+@app.route("/")
+def homepage():
+
+    return render_template('UserLogin.html')
+
+@app.route("/alogin")
+def alogin():
+
+    return render_template('AdminLogin.html')
+
+@app.route("/NewUser")
+def NewUser():
+
+    return render_template('NewUser.html')
+
+@app.route("/RNewUser", methods=['GET', 'POST'])
+def RNewUser():
+    if request.method == 'POST':
+
+        name1 = request.form['name']
+        gender1 = request.form['gender']
+        Age = request.form['age']
+        email = request.form['email']
+        address = request.form['address']
+        pnumber = request.form['phone']
+        uname = request.form['uname']
+        password = request.form['psw']
+
+        conn = ibm_db.connect(dsn, "", "")
+
+        insertQuery = "INSERT INTO regtb VALUES ('" + name1 + "','" + gender1 + "','" + Age + "','" + email + "','" + pnumber + "','" + address + "','" + uname + "','" + password + "')"
+        insert_table = ibm_db.exec_immediate (conn, insertQuery)
+        print(insert_table)
+
+
+
+
+
+
+    return render_template('userlogin.html')
+
+
+
+
+
+
+
+@app.route("/AdminHome")
+def AdminHome():
+
+    conn = ibm_db.connect(dsn, "", "")
+    pd_conn = ibm_db_dbi.Connection(conn)
+
+    selectQuery = "SELECT * from regtb "
+    dataframe = pandas.read_sql(selectQuery, pd_conn)
+
+    dataframe.to_sql('Employee_Data',
+                     con=engine,
+                     if_exists='append')
+
+    # run a sql query
+    data = engine.execute("SELECT * FROM Employee_Data").fetchall()
+
+    return render_template('AdminHome.html', data=data)
+
+
+
+
+@app.route("/userlogin", methods=['GET', 'POST'])
+def userlogin():
+    error = None
+    if request.method == 'POST':
+        username = request.form['uname']
+        password = request.form['password']
+        session['uname'] = request.form['uname']
+
+
+        conn = ibm_db.connect(dsn, "", "")
+        pd_conn = ibm_db_dbi.Connection(conn)
+
+        selectQuery = "SELECT * from regtb where uname='" + username + "' and password='" + password + "'"
+        dataframe = pandas.read_sql(selectQuery, pd_conn)
+
+        if dataframe.empty:
+            data1 = 'Username or Password is wrong'
+            return render_template('goback.html', data=data1)
+        else:
+            print("Login")
+            selectQuery = "SELECT * from regtb where uname='" + username + "' and password='" + password + "'"
+            dataframe = pandas.read_sql(selectQuery, pd_conn)
+
+
+
+            dataframe.to_sql('Employee_Data',
+                       con=engine,
+                       if_exists='append')
+
+            # run a sql query
+            print(engine.execute("SELECT * FROM Employee_Data").fetchall())
+
+            return render_template('index.html', data=engine.execute("SELECT * FROM Employee_Data").fetchall())
+
+@app.route("/adminlogin", methods=['GET', 'POST'])
+def adminlogin():
+    error = None
+    if request.method == 'POST':
+        username = request.form['uname']
+        password = request.form['password']
+
+
+        conn = ibm_db.connect(dsn, "", "")
+        pd_conn = ibm_db_dbi.Connection(conn)
+
+        selectQuery = "SELECT * from admintb where USERNAME='" + username + "' and PASSWORD='" + password + "'"
+        dataframe = pandas.read_sql(selectQuery, pd_conn)
+
+        if dataframe.empty:
+            data1 = 'Username or Password is wrong'
+            return render_template('goback.html', data=data1)
+        else:
+            print("Login")
+            selectQuery = "SELECT * from regtb "
+            dataframe = pandas.read_sql(selectQuery, pd_conn)
+
+            dataframe.to_sql('Employee_Data', con=engine,if_exists='append')
+
+            # run a sql query
+            print(engine.execute("SELECT * FROM Employee_Data").fetchall())
+
+        return render_template('AdminHome.html', data=engine.execute("SELECT * FROM Employee_Data").fetchall())
+
+
+
+
+
+
+@app.route('/news', methods=['POST'])
+def submit_data():
+    vals = request.form['news-source']
+    id = vals.split('_')[0]
+    name = vals.split('_')[1]
+    articles = news.main(id)
+    print(articles)
+    return render_template('news.html', articles=articles, name=name)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
